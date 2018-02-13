e881aaa41e4b93b4587f09986068d301a4d0b09e
Ermanno Pirotta <ermanno.pirotta@gmail.com>
2018-02-13 13:30:58 +0100

    add support adding watermark to multiple files, using unix path

--- watermark_image.py
+++ watermark_image.py
@@ -1,25 +1,39 @@
 import argparse
+import glob
+import re
+
 from img_util import add_watermark
 
-def main(args):
+def is_unix_path(path):
+    match =re.match(r'.*\*+.*',path)
+    if (match):
+        return True
+    else:
+        return False
 
-    output_file =args.output
-    input_file = args.input
+def build_output_file_name(input_name):
+    return input_name + '_with_watermark.png'
+
+def main(args):
+    input_path = args.input
     text = args.text
     color = args.color
     position = args.position
 
-    add_watermark(input_file, output_file, text, color, position)
+    if is_unix_path(input_path):
+        print('input recognised as unix path: processing multiple elements')
+        for file_name in glob.glob(input_path):
+            print('processing file %s' % file_name)
+            add_watermark(file_name, build_output_file_name(file_name), text, color, position)
+    else:
+        add_watermark(input_path, build_output_file_name(input_path), text, color, position)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Watermark image maker')
 
     parser.add_argument('--input', '-i', default=None, required=True,type=str,
-                        help='input file name')
-
-    parser.add_argument('--output', '-o', default=None, required=True,type=str,
-                        help='output file name without extension')
+                        help='input file name. Supports single file or multiple file if path is in the form of unix pattern. I.e. ./input_dir/*.jpg')
 
     parser.add_argument('--text', '-t', default=0, required=True,  type=str,
                         help='Text to write')
